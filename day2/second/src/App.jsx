cc3b3d7f36bfa646d0a442579c8b7775a8c5a0c5
VishalBorse444 <vp346806@gmail.com>
2026-05-24 12:25:33 +0530

    Added all react practice projects

---
+++ day2/second/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import { useState } from 'react'  
+
+function App() {
+const[count,setcount]= useState(0);
+
+const incrcount=()=>{
+  //setcount(count + 1);//simple to increse one count 
+  setcount(prevcount => prevcount + 1); // asked in the interview to increase value multiple times
+  setcount(prevcount => prevcount + 1);
+  setcount(prevcount => prevcount + 1);
+  setcount(prevcount => prevcount + 1);
+
+}
+ const decrese=()=>{
+  setcount(count-1)
+ }
+
+  return (
+    <>
+     <h1 className='bg-green-400 text-black p-4 rounded-xl'>tailwind test</h1>
+     <button onClick={incrcount}>incrcount {count}</button>
+      <button onClick={ decrese}>deccount {count}</button>
+    </>
+  )
+}
+
+export default App
